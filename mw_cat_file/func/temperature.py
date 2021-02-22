385686335bbdca50ac7b55857eb694f5918d512c
rberthin <roxanne.berthin1@gmail.com>
2021-02-22 10:17:07 +0100

    directory for functions

---
+++ mw_cat_file/func/temperature.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Roxanne Berthin <roxanne.berthin@sorbonne-universite.fr>, version 16/02/2021
+
+
+def cat_temperature(dir_list, step, freq):
+    fout = open('temperature_cat_1-'+str(len(dir_list))+'.out', 'w')
+    header = '# Temperature in K\n# ----------------\n# step temperature (K)\n'
+    fout.write(header)
+    count = 0
+    for i in range (len(dir_list)):
+        fin = open(str(dir_list[i])+'/temperature.out','r')
+        print('    directory '+str(i+1)+'/'+str(len(dir_list)))
+        line = fin.readline()
+        line = fin.readline()
+        line = fin.readline()
+
+        if i < (len(dir_list)-1):
+            for j in range(int(step[i]/freq)):
+                line = fin.readline()
+                fout.write('{0:8s}    {1}\n'.format(
+                          str(int(line.split()[0])+count), line.split()[1]))
+        else : 
+            for j in range(int(step[i]/freq)+1):
+                line = fin.readline()
+                fout.write('{0:8s}    {1}\n'.format(
+                           str(int(line.split()[0])+count), float(line.split()[1])))
+
+        count = count + step[i]
+
--- mw_cat_file/mw_cat_file.py
+++ mw_cat_file/mw_cat_file.py
@@ -6,6 +6,7 @@
 import sys
 import argparse
 import numpy as np
+from func.temperature import cat_temperature
 
 #******************************************************************************************
 #******************************************************************************************
@@ -15,34 +16,6 @@ def read_data(file):
             if (line.lstrip()).startswith("num_atoms"):
                 nbatoms = float(line.split()[1])
     return int(nbatoms)
-
-#******************************************************************************************
-#******************************************************************************************
-
-def cat_temperature(dir_list, step, freq):
-    fout = open('temperature_cat_1-'+str(len(dir_list))+'.out', 'w')
-    header = '# Temperature in K\n# ----------------\n# step temperature (K)\n'
-    fout.write(header)
-    count = 0
-    for i in range (len(dir_list)):
-        fin = open(str(dir_list[i])+'/temperature.out','r')
-        print('    directory '+str(i+1)+'/'+str(len(dir_list)))
-        line = fin.readline()
-        line = fin.readline()
-        line = fin.readline()
-
-        if i < (len(dir_list)-1):
-            for j in range(int(step[i]/freq)):
-                line = fin.readline()
-                fout.write('{0:8s}    {1}\n'.format(
-                          str(int(line.split()[0])+count), line.split()[1]))
-        else : 
-            for j in range(int(step[i]/freq)+1):
-                line = fin.readline()
-                fout.write('{0:8s}    {1}\n'.format(
-                           str(int(line.split()[0])+count), float(line.split()[1])))
-
-        count = count + step[i]
 
 #******************************************************************************************
 #******************************************************************************************
@@ -124,7 +97,9 @@ def main():
     print('**********************************************\n')
     print(' I can cat :')
     print('  - temperature.out   (temperature)')
-    print('  - trajectories.xyz  (xyz)\n') 
+    print('  - trajectories.xyz  (xyz)\n')
+    print('  - trajectories.out  (traj)\n')
+ 
     parser = argparse.ArgumentParser(
         description = '  ')
     parser.add_argument('-f', '--propertie', default = '', help = 'types of '\
