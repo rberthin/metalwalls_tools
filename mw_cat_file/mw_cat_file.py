7e3e4878db8ebc7a4b2dad2cbd671a43d6005f1b
rberthin <roxanne.berthin1@gmail.com>
2021-03-02 17:08:07 +0100

    update

--- mw_cat_file/mw_cat_file.py
+++ mw_cat_file/mw_cat_file.py
@@ -15,7 +15,6 @@ from func.dipoles import cat_dip
 from func.stress import cat_stress
 from func.pressure import cat_pressure
 from func.box import cat_box
-from func.forces import cat_forces
 
 def main():
     print('\n**********************************************')
@@ -32,7 +31,6 @@ def main():
     print('  - stress_tensor.out   (stess_tensor)')
     print('  - pressure.out        (pressure)')
     print('  - box_parameters.out  (box_parameters)')
-    print('  - forces.out          (forces)\n')
 
     parser = argparse.ArgumentParser(
 
@@ -118,12 +116,6 @@ def main():
              print('** Starting files box_parameters.out **')
              cat_box(dir_list, step, write_output[prop])
              print('box_parameters :  DONE\n')
-
-        if prop_list[prop] == 'forces':
-            print('** Starting files forces.out **')
-            nat = read_data(str(dir_list[0])+'/data.inpt','all')
-            cat_forces(dir_list, step, nat, write_output[prop])
-            print('forces :  DONE')
 
     print('**********************************************')
     print('*********    !!   ALL  DONE   !!     *********')
